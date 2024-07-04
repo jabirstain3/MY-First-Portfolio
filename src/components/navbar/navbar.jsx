a4ce153457d00f016bd16fe74db573d724575bba
jabirstain3 <mrjabir229@gmail.com>
2024-07-05 03:32:30 +0600

    test

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -7,7 +7,7 @@ const navbar = () => {
         
         <div className="bg-slate-600 dark:bg-gray-500  flex justify-between  items-center flex-wrap px-3 py-2 sm:px-6 sm:py-4 ">
             <a className="" href="/">
-                <img className="w-12 sm:w-16" src="../../../src/assets/images/LOGO_1.png" alt="somthing is loading" />
+                <img className="w-12 sm:w-16" src="https://ibb.co/GsD5NhP" alt="somthing is loading" />
             </a>
             <Menu></Menu>
         </div>
